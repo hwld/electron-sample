b3e257e52704572f220d22bc9dcdd733055ab4c6
hwld <0701hw@gmail.com>
2024-11-13 00:26:48 +0900

    tailwind

--- src/renderer/src/components/main-app.tsx
+++ src/renderer/src/components/main-app.tsx
@@ -14,8 +14,8 @@ function MainApp(): JSX.Element {
   }, [])
 
   return (
-    <div>
-      <div>
+    <div className="bg-neutral-900 min-h-[100dvh] text-neutral-100 gap-2 pt-10 px-6">
+      <div className="text-4xl">
         {tasks.map((t, i) => {
           return <div key={i}>{t.title}</div>
         })}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/renderer/input-panel.html',
+    './src/renderer/main-window.html',
+    './src/renderer/**/*.{ts,tsx}'
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
